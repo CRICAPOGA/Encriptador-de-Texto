7785af1f197c8d99b0d75a04bcbb7e2b9359240b
CRICAPOGA <camilochoposada2@gmail.com>
2024-06-30 10:53:46 -0500

    Implementando el metodo de enccriptación

--- Interactividad.js
+++ Interactividad.js
@@ -1,4 +1,24 @@
-let anchoPantalla = screen.width;
+/*
+METODO DE ENCRIPTACIÓN
+El metodo de encriptación utilizado es una modificación propia
+del algoritmo de encriptación Cesar; La encriptación Cesar consiste
+en desplazar los caracteres del mensaje una cierta cantidad de
+espacios.
+Por ejemplo.
+Clave: 3
+Entrada: A
+    A -> +1 = B
+    A -> +2 = C
+    A -> +3 = D
+Salida: D
+
+Mi modificación consiste en darle un valor de orden desde 0 a 699
+a cada caracter.
+Reglas:
+- Si el valor del caracter es par que se desplace 4 caracteres adelante
+- Si el valor del caracter es impar que se desplace 4 caracteres atras
+
+*/
 
 function encriptar() {
   //Encriptación Cesar (4)
@@ -11,7 +31,12 @@ function encriptar() {
     //Pasar de caracteres a valores Ascii
     let textoAscii = texto.split("");
     for (let i = 0; i < textoAscii.length; i++) {
-      textoAscii[i] = textoAscii[i].charCodeAt(0) + 4; //Almacena el valor en Ascii + 4 en el arreglo
+      //Metodo de encriptación
+      if (i % 2 == 0) {
+        textoAscii[i] = textoAscii[i].charCodeAt(0) + 4; //Almacena el valor en Ascii + 4 en el arreglo
+      } else {
+        textoAscii[i] = textoAscii[i].charCodeAt(0) - 4; //Almacena el valor en Ascii + 4 en el arreglo
+      }
     }
 
     //Pasar de valores Ascii a Letra
@@ -31,11 +56,11 @@ function encriptar() {
 
       document.querySelector("#salida").innerHTML = texto;
     } else {
-        document.querySelector("#subtSalida").style.display = "flex";
-        //Solo muentra la imagen en caso de que sea en pc
-        if (anchoPantalla > 768) {
-            document.querySelector("#imagen").style.display = "flex";
-        }
+      document.querySelector("#subtSalida").style.display = "flex";
+      //Solo muentra la imagen en caso de que sea en pc
+      if (anchoPantalla > 768) {
+        document.querySelector("#imagen").style.display = "flex";
+      }
       document.querySelector("#botonCopiar").style.visibility = "hidden";
       document.querySelector("aside").style.alignItems = "center";
       document.querySelector("aside").style.justifyContent = "center";
@@ -58,7 +83,12 @@ function desencriptar() {
     //Pasar de caracteres a valores Ascii
     let textoAscii = texto.split("");
     for (let i = 0; i < textoAscii.length; i++) {
-      textoAscii[i] = textoAscii[i].charCodeAt(0) - 4; //Almacena el valor en Ascii + 4 en el arreglo
+      //Metodo de encriptación
+      if (i % 2 == 0) {
+        textoAscii[i] = textoAscii[i].charCodeAt(0) - 4; //Almacena el valor en Ascii + 4 en el arreglo
+      } else {
+        textoAscii[i] = textoAscii[i].charCodeAt(0) + 4; //Almacena el valor en Ascii + 4 en el arreglo
+      }
     }
 
     //Pasar de valores Ascii a Letra
